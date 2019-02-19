fbfd562eb4dc4ea7f460c01e4b7195e29e82500b
chebotarmv <chebotarmv@gmail.com>
2019-02-19 11:00:52 +0300

    Vk_bot first commit

---
+++ vk_bot/bot_config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Configuration file for VK bot
+"""
+# token issued on the VK group web-administration page
+token = '052b7097d0423be7631abd0f67367c7a18b67c321a14bb8281e0d56f996205dfb715f9fdc90579008aa9b'
+# confirmation token issued on the VK group web-administration page in "Callback API" section
+confirmation_token = 'd7fdcbb4'
+# secret key for cross site request forgery protection. It will be in each VK server request
+secret_key = 'aaQ13axAPQEcczQa'
---
+++ vk_bot/views.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from .bot_config import *
+
+import json, vk
+
+
+@csrf_exempt
+def index(request):
+    if (request.method == "POST"):
+        data = json.loads(request.body)
+        if (data['secret'] == secret_key):
+            if (data['type'] == 'confirmation'):
+                return HttpResponse(confirmation_token, content_type="text/plain", status=200)
+            if (data['type'] == 'message_new'):
+                session = vk.Session()
+                api = vk.API(session, v=5.5)
+                user_id = data['object']['user_id']
+                api.messages.send(access_token = token, user_id = str(user_id), message = "Hello, I'm bot!")
+                return HttpResponse('ok', content_type="text/plain", status=200)
+    else:
+        return HttpResponse('see you :)')
